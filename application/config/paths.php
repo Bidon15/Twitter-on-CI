a00534d4cbda1e42c0fac4c5138bd08e85a4d1aa
Bidon <braveryandglory@gmail.com>
2012-06-14 16:35:33 +0300

    ordering by desc  for showing all users. Viewing your own profile without changing the email address and password. Coding o uploading photos and putting a gravatar if there is no avatar chosen

---
+++ application/config/paths.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Bidon
+ * Date: 14.06.12
+ * Time: 15:41
+ * To change this template use File | Settings | File Templates.
+ */
+$config['path_users_images'] = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR;
+$config['url_users_images'] = "uploads/";
+
+
+?>
