c04d8acfd1e3232ab349e1b90a16e0c42ada0b86
Bidon <braveryandglory@gmail.com>
2012-06-20 19:47:59 +0300

    Finished the functionality of the site. Working on styling

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -9,6 +9,16 @@
     <div class="navbar-inner">
         <div class="container">
             <a class="brand" href="sessions/signin">Twitter on CI</a>
+            <div class="nav-collapse">
+                <ul class="nav pull-right">
+                    <li>
+                        <?php echo anchor('users/index','Home')?>
+                    </li>
+                    <li>
+                        <?php echo anchor('sessions/help','Help')?>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
