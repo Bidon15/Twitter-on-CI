508db09e60b70c1e71507b26c922dd4b9f9b317e
Bidon <braveryandglory@gmail.com>
2012-06-20 14:23:43 +0300

    Repairing bugs

---
+++ application/views/users/following.php
@@ -0,0 +1,29 @@
+<div id='user'>
+    <?php echo $user['username'];?><br/><?php echo anchor('users/show/'.$this->session->userdata('user_id'),'Home')?><br/>
+    <?php if ($user['image'] == NULL) { ?> <img
+    src="http://www.gravatar.com/avatar/<?php echo md5(strtolower(trim($user['email_address'])));?>"/>
+    <?php } else echo img($this->config->item('url_users_images') . $user['image']); ?></p>
+    <?php echo anchor('users/following/' . $user['id'] . '/' . $count_followings, 'Following:  ' . $count_followings)  ?>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <?php echo anchor('users/followers/' . $count_followers, 'Followers:  ' . $count_followers)?><!--<br/>-->
+
+</div>
+
+<div id="following">
+    <h2><?php echo $title;?></h2>
+    <?php echo anchor('users/edit', 'Edit My Profile');?><br/>
+    <?php
+    foreach ($results as $users):?>
+
+
+        <p>
+            <?php if ($users['image'] == NULL) { ?> <img
+            src="http://www.gravatar.com/avatar/<?php echo md5(strtolower(trim($users['email_address'])));?>"/>
+            <?php } else echo img($this->config->item('url_users_images') . $users['image']); ?><br/>
+            <?php
+            echo anchor('users/show/' . $users['id'], $users['username']);
+            echo "<br/>";
+            ?></p>
+
+        <?php endforeach;?>
+</div>
